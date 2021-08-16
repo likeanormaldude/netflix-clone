eeb2212554ace35f21edae5d331058b67fa6431b
Deivid <deivid.likeanormaldude@hotmail.com>
2021-08-16 12:28:26 -0300

    Implemented top navbar and trailers

--- src/Row.js
+++ src/Row.js
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from "react";
 import axios from "./axios";
 import "./Row.css";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
+import { APIKEY } from "./requests";
 
 const base_url = "https://image.tmdb.org/t/p/w500/";
 
 function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(fetchUrl);
@@ -19,6 +23,31 @@ function Row({ title, fetchUrl, isLargeRow }) {
 
   //   console.table(movies);
 
+  const opts = {
+    height: "390",
+    width: "100%",
+    playersVars: {
+      autoplay: 1,
+    },
+  };
+
+  const mtOptions = {
+    apiKey: APIKEY,
+  };
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || "", mtOptions)
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
+    }
+  };
+
   return (
     <div className="row">
       <h2>{title}</h2>
@@ -27,6 +56,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
         {movies.map((movie) => (
           <img
             key={movie.id}
+            onClick={() => handleClick(movie)}
             className={`row__poster ${isLargeRow && "row__posterLarge"}`}
             src={`${base_url}${
               isLargeRow ? movie.poster_path : movie.backdrop_path
@@ -35,6 +65,8 @@ function Row({ title, fetchUrl, isLargeRow }) {
           />
         ))}
       </div>
+
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 }
--- src/requests.js
+++ src/requests.js
@@ -11,4 +11,6 @@ const requests = {
   fetchDocumentaries: `/discover/movie?api_key=${APIKEY}&with_genres=99`,
 };
 
+export { APIKEY };
+
 export default requests;
